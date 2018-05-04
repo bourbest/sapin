df65715938c50acf43136466e8417999403a8ea1
Stéphane Bourbeau <stephane.bourbeau@telus.com>
2018-05-03 21:54:06 -0400

    reduce number of params in most validate functions

--- src/validate.js
+++ src/validate.js
@@ -1,24 +1,30 @@
 import {forEach, isArray, isEmpty} from 'lodash'
 import {PropertyDefinition, ValueTypes} from './types'
-import Schema from './Schema'
 
-const runFunctions = function (value, validations, getter, siblings, entity, params) {
+const runFunctions = function (value, validations, getter, siblings, globals) {
   let err = null
   if (!isArray(validations)) {
     validations = [validations]
   }
+  const params = {
+    value,
+    getter,
+    siblings,
+    entity: globals.entity,
+    params: globals.params
+  }
   for (let i = 0; !err && i < validations.length; i++) {
     const validation = validations[i]
-    err = validation({value, getter, siblings, entity, params})
+    err = validation(params)
   }
   return err
 }
 
-const validateCollectionOfObjects = function (objectsCollection, propertyValidator, entity, params, logUnknownProperties) {
+const validateCollectionOfObjects = function (objectsCollection, propertyValidator, globals) {
   const errors = {}
 
   forEach(objectsCollection, (object, idx) => {
-    const err = validateObject(objectsCollection[idx], propertyValidator.validators, objectsCollection[idx], entity, params, logUnknownProperties)
+    const err = validateObject(objectsCollection[idx], propertyValidator.validators, objectsCollection[idx], globals)
     if (err) {
       errors[idx.toString()] = err
     }
@@ -26,11 +32,11 @@ const validateCollectionOfObjects = function (objectsCollection, propertyValidat
   return isEmpty(errors) ? null : errors
 }
 
-const validateCollectionOfValues = function (valuesCollection, propDefinition, siblings, entity, params) {
+const validateCollectionOfValues = function (valuesCollection, propDefinition, siblings, globals) {
   const errors = {}
   forEach(valuesCollection, (value, idx) => {
     const valueToValidate = valuesCollection[idx]
-    const err = runFunctions(valueToValidate, propDefinition.validators.validators, propDefinition.validators.getter, valuesCollection, entity, params)
+    const err = runFunctions(valueToValidate, propDefinition.validators.validators, propDefinition.validators.getter, valuesCollection, globals)
     if (err) {
       errors[idx.toString()] = err
     }
@@ -39,43 +45,43 @@ const validateCollectionOfValues = function (valuesCollection, propDefinition, s
   return isEmpty(errors) ? null : errors
 }
 
-const executeValidator = function (propValue, propDefinition, siblings, entity, params, logUnknownProperties) {
+const executeValidator = function (propValue, propDefinition, siblings, globals) {
   if (propDefinition.valueType === ValueTypes.value) {
-    return runFunctions(propValue, propDefinition.validators, propDefinition.getter, siblings, entity, params)
+    return runFunctions(propValue, propDefinition.validators, propDefinition.getter, siblings, globals)
   } else {
     // execute the value validation first
     if (propDefinition.collectionValidator) {
-      const _error = runFunctions(propValue, propDefinition.collectionValidator, propDefinition.getter, siblings, entity, params)
+      const _error = runFunctions(propValue, propDefinition.collectionValidator, propDefinition.getter, siblings, globals)
       if (_error) {
         return {_error}
       }
     }
 
     if (propDefinition.valueType === ValueTypes.collectionOfObjects) {
-      return validateCollectionOfObjects(propValue, propDefinition, entity, params, logUnknownProperties)
+      return validateCollectionOfObjects(propValue, propDefinition, globals)
     } else {
-      return validateCollectionOfValues(propValue, propDefinition, siblings, entity, params)
+      return validateCollectionOfValues(propValue, propDefinition, siblings, globals)
     }
   }
 }
 
-const validateObject = function (object, schema, siblings, entity, params, logUnknownProperties) {
+const validateObject = function (object, schema, siblings, globals) {
   const errors = {}
   for (let propName in schema) {
     const propValue = object ? object[propName] : null
     const propDefinition = schema[propName]
     let propErrors
     if (propDefinition instanceof PropertyDefinition) {
-      propErrors = executeValidator(propValue, propDefinition, siblings, entity, params, logUnknownProperties)
+      propErrors = executeValidator(propValue, propDefinition, siblings, globals)
     } else {
-      propErrors = validateObject(propValue, propDefinition, propValue, entity, params, logUnknownProperties)
+      propErrors = validateObject(propValue, propDefinition, propValue, globals)
     }
     if (propErrors) {
       errors[propName] = propErrors
     }
   }
 
-  if (logUnknownProperties) {
+  if (globals.logUnknownProperties) {
     for (let propName in object) {
       if (!schema[propName]) {
         errors[propName] = 'sapin.unknownProperty'
@@ -87,5 +93,10 @@ const validateObject = function (object, schema, siblings, entity, params, logUn
 }
 
 export const validate = function (values, schema, params = null, logUnknownProperties = false) {
-  return validateObject(values, schema.schema, values, values, params, logUnknownProperties)
+  const globals = {
+    entity: values,
+    params,
+    logUnknownProperties
+  }
+  return validateObject(values, schema.schema, values, globals)
 }
